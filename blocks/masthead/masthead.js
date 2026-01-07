410e52060e50fbfc11e5aaca6b2c2fd851412929
Nguyen Tran Trung <nguyentrantrung@hotmail.com>
2026-01-07 17:46:47 +0800

    masthead block

---
+++ blocks/masthead/masthead.js
@@ -0,0 +1,9 @@
+export default function decorate(block) {
+    const blocks = block.querySelectorAll(':scope > div');
+
+    blocks[0].classList.add('desktop-section');
+    blocks[1].classList.add('mobile-section');
+    blocks[2].classList.add('desc');
+
+    block.append(block);
+}
