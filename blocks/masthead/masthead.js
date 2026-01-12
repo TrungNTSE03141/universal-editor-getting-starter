ce5168448b79eca9b3d3b8a71cc5691e6249a1c6
Nguyen Tran Trung <nguyentrantrung@hotmail.com>
2026-01-12 13:52:37 +0800

    test

--- blocks/masthead/masthead.js
+++ blocks/masthead/masthead.js
@@ -3,5 +3,22 @@ export default function decorate(block) {
 
     blocks[0].classList.add('desktop-section');
     blocks[1].classList.add('mobile-section');
-    blocks[2].classList.add('desc');
+
+    const desc = document.createElement('div');
+    desc.classList.add('desc');
+
+    desc.appendChild(blocks[2]);
+    desc.appendChild(blocks[3]);
+
+    const ctaLink = document.createElement('a');
+    ctaLink.className = "button";
+    ctaLink.href = blocks[5].innerText.trim();
+    ctaLink.textContent = blocks[4].innerText.trim();
+
+    desc.appendChild(ctaLink);
+
+    blocks[4].remove();
+    blocks[5].remove();
+
+    block.appendChild(desc);
 }
